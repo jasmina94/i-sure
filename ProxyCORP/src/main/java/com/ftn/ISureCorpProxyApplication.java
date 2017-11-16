a5b0ef5f4706e688ebd85d1b393ee7c3200c41d6
Jasmina <jasmina.eminovski@gmail.com>
2017-11-16 22:37:07 +0100

    Created Proxy apps

---
+++ ProxyCORP/src/main/java/com/ftn/ISureCorpProxyApplication.java
@@ -0,0 +1,12 @@
+package com.ftn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISureCorpProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISureCorpProxyApplication.class, args);
+	}
+}
