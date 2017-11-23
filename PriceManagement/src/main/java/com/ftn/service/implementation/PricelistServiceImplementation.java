5b0cff58701e630a94166e91d55dd7e41ace00a0
= <sh4deify@gmail.com>
2017-11-23 12:36:08 +0100

    Added button icon.

--- PriceManagement/src/main/java/com/ftn/service/implementation/PricelistServiceImplementation.java
+++ PriceManagement/src/main/java/com/ftn/service/implementation/PricelistServiceImplementation.java
@@ -2,38 +2,40 @@ package com.ftn.service.implementation;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.ftn.model.dto.PricelistDTO;
 import com.ftn.service.PricelistService;
 
+@Service
 public class PricelistServiceImplementation implements PricelistService{
 
 	@Override
 	public List<PricelistDTO> findAll() {
-		// TODO Auto-generated method stub
+		System.out.println("find all");
 		return null;
 	}
 
 	@Override
 	public PricelistDTO findById(Long id) {
-		// TODO Auto-generated method stub
+		System.out.println("find one");
 		return null;
 	}
 
 	@Override
 	public PricelistDTO create(PricelistDTO pricelistDTO) {
-		// TODO Auto-generated method stub
+		System.out.println("create");
 		return null;
 	}
 
 	@Override
 	public PricelistDTO update(PricelistDTO pricelistDTO) {
-		// TODO Auto-generated method stub
+		System.out.println("update");
 		return null;
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("delete");
 	}
 }
