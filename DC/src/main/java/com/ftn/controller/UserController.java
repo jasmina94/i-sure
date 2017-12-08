b020e522c045e39d214dbab1df0497cfaf096dee
= <sh4deify@gmail.com>
2017-12-08 14:46:57 +0100

    Adding button icon.

--- DC/src/main/java/com/ftn/controller/UserController.java
+++ DC/src/main/java/com/ftn/controller/UserController.java
@@ -5,11 +5,18 @@ import com.ftn.exception.BadRequestException;
 import com.ftn.model.dto.UserDTO;
 import com.ftn.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
@@ -61,5 +68,40 @@ public class UserController {
     public ResponseEntity delete(@PathVariable Long id) {
         userService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json")
+    public String testReceivingGet(HttpServletRequest request) {
+        System.out.println("Request come from address: " + request.getRemoteAddr());
+        return "Response <3";
+    }
+    
+    @RequestMapping(value = "/testSend", method = RequestMethod.GET, produces = "application/json")
+    public void testSendingGet(){
+        String url = "http://localhost:"+8081+"/test"; //needs to know address and port to send message to
+        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        //Building query with params
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("name", "Jasmina")
+                .queryParam("lastname", "Eminovski");
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        HttpEntity<String> response = restTemplate.exchange(
+                builder.build().encode().toUri(),
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        if(response == null) {
+            System.out.println("Response is null.");
+        }else {
+            System.out.println("Response is not null.");
+            System.out.println(response.getBody());
+        }
     }
 }
