8635bf4d2855f1cea69f6371f74d770e762d4cc0
Desanka <desankatodic@yahoo.com>
2017-12-20 00:03:56 +0100

    Added link to corporative email.

--- i-sure-sales/src/main/webapp/indexController.js
+++ i-sure-sales/src/main/webapp/indexController.js
@@ -6,12 +6,15 @@
     'use strict';
     var app = angular.module('iSure');
 
-    app.controller('indexController', function ($scope, ngNotify, $state) {
+    app.controller('indexController', function ($scope, ngNotify, $state, $http) {
         var indexVm = this;
 
         indexVm.getInsurance = function() {
             $state.go('insurancePage');
         }
+        
+        indexVm.email = "http://localhost:80/squirrelmail/src/login.php";
+
     });
 
 })();
