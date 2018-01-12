febbdbebd819b5d8c8c5ecb1613829f213b09c74
ProgrammerToBe <teauvranju@hotmail.com>
2018-01-12 20:48:28 +0100

    Proxy communication added.

--- ISurePortal/src/main/webapp/components/insuranceStepper/insuranceService.js
+++ ISurePortal/src/main/webapp/components/insuranceStepper/insuranceService.js
@@ -4,8 +4,9 @@
 (function() {
     'use strict';
     var url = "http://localhost:8090/insurance/";
-    var urlTransactions = "http://localhost:9030/transactions/"
-    var urlInquiries = "http://localhost:9030/inquiries/"
+    var urlTransactions = "http://localhost:8090/transactions/"
+    //var urlTransactions = "http://localhost:9030/transactions/"
+    //var urlInquiries = "http://localhost:9030/inquiries/"
 
     angular.module('iSure').factory('insuranceService', function insuranceService($http) {
 
@@ -18,12 +19,12 @@
             }
         
         insuranceService.createTransaction = function(transactionDTO) {
-           return $http.post(urlTransactions,transactionDTO);
+           return $http.post(urlTransactions + "transactions",transactionDTO);
         	//return $http.get(urlTransactions + "transactions");
             }
         
         insuranceService.createInquiry = function(transactionDTO) {
-            return $http.post(urlInquiries,transactionDTO);
+            return $http.post(urlTransactions + "inquiries",transactionDTO);
          	//return $http.get(urlTransactions + "transactions");
              }
         return insuranceService;
