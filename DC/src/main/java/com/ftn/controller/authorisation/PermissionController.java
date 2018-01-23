6f9e723648d8682c60b7747ab05afeecb5eb9edd
ProgrammerToBe <teauvranju@hotmail.com>
2018-01-23 11:15:13 +0100

    Templates fixed.

--- DC/src/main/java/com/ftn/controller/authorisation/PermissionController.java
+++ DC/src/main/java/com/ftn/controller/authorisation/PermissionController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -86,7 +85,7 @@ public class PermissionController {
 	}
 	
 	//@CustomAnnotation(value = "FIND_ALL_PERMISSION")
-	@PreAuthorize("hasRole('admin')")
+	//@PreAuthorize("hasRole('admin')")
 	@GetMapping(value = "/allPermissions")
 	public ResponseEntity<Collection<Permission>> allPermissions() throws Exception {
 
--- DC/src/main/java/com/ftn/security/config/SecurityConfig.java
+++ DC/src/main/java/com/ftn/security/config/SecurityConfig.java
@@ -1,63 +1,63 @@
 package com.ftn.security.config;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-
-@Configuration
-@EnableWebSecurity
-@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
-@EnableGlobalMethodSecurity(prePostEnabled=true)
-class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
-{
-   
-   @Autowired
-   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-      KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-      keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-      auth.authenticationProvider(keycloakAuthenticationProvider);
-   }
-   @Bean
-   public org.keycloak.adapters.KeycloakConfigResolver KeycloakConfigResolver() {
-      return new KeycloakSpringBootConfigResolver();
-   }
-   @Bean
-   @Override
-   protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-      return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-   }
-   @Override
-   protected void configure(HttpSecurity http) throws Exception
-   {
-      super.configure(http);
-      http
-            .authorizeRequests()
-            .antMatchers("/").authenticated().anyRequest().permitAll().and()
-            .httpBasic().and()
-            //.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
-            //.csrf().csrfTokenRepository(csrfTokenRepository());
-            .csrf().disable();
-   }
-   
-   private CsrfTokenRepository csrfTokenRepository() {
-	   HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-	   repository.setHeaderName("X-XSRF-TOKEN");
-	   return repository;
-   }
-}
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+//import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+//import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+//import org.springframework.security.core.session.SessionRegistryImpl;
+//import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+//import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+//import org.springframework.security.web.csrf.CsrfFilter;
+//import org.springframework.security.web.csrf.CsrfTokenRepository;
+//import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+//
+//@Configuration
+//@EnableWebSecurity
+//@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+//@EnableGlobalMethodSecurity(prePostEnabled=true)
+//class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
+//{
+//   
+//   @Autowired
+//   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//      KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+//      keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+//      auth.authenticationProvider(keycloakAuthenticationProvider);
+//   }
+//   @Bean
+//   public org.keycloak.adapters.KeycloakConfigResolver KeycloakConfigResolver() {
+//      return new KeycloakSpringBootConfigResolver();
+//   }
+//   @Bean
+//   @Override
+//   protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+//      return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+//   }
+//   @Override
+//   protected void configure(HttpSecurity http) throws Exception
+//   {
+//      super.configure(http);
+//      http
+//            .authorizeRequests()
+//            .antMatchers("/").authenticated().anyRequest().permitAll().and()
+//            .httpBasic().and()
+//            //.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+//            //.csrf().csrfTokenRepository(csrfTokenRepository());
+//            .csrf().disable();
+//   }
+//   
+//   private CsrfTokenRepository csrfTokenRepository() {
+//	   HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+//	   repository.setHeaderName("X-XSRF-TOKEN");
+//	   return repository;
+//   }
+//}
